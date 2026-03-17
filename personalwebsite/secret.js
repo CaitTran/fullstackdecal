ee47a29d87a74215d67d30e5bebad6c167accddb
Caitlin Tran <caitlintran@wifi-10-43-138-68.wifi.berkeley.edu>
2026-03-16 19:20:23 -0700

    personal website

---
+++ personalwebsite/secret.js
@@ -0,0 +1,6 @@
+// ── BACK BUTTON ──
+if (document.getElementById('back-btn')) {
+  document.getElementById('back-btn').addEventListener('click', () => {
+    window.location.href = "index.html";
+  });
+}
